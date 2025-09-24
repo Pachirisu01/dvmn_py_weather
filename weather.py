114896d74acac86df15a9c25487fbe9119d89022
Polina Vasilyeva <Polinova31@gmail.com>
2025-09-24 15:21:04 +0300

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,13 @@
+import requests
+
+
+cities = ['london', 'svo', 'Череповец'] 
+url_template = 'https://wttr.in/{}'
+payload = {"mMnTqu": "", "lang":"ru"}
+
+
+for city in cities:
+	url = url_template.format(city)	
+	response = requests.get(url, params=payload)
+	response.raise_for_status()
+	print(response.text) 
