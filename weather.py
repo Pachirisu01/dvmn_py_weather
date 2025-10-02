76855cc94a164537f2f583a1fc23f855c7c470ad
Polina Vasilyeva <Polinova31@gmail.com>
2025-10-02 19:54:38 +0300

    Add files via upload

--- weather.py
+++ weather.py
@@ -6,8 +6,13 @@ url_template = 'https://wttr.in/{}'
 payload = {"mMnTqu": "", "lang":"ru"}
 
 
-for city in cities:
-	url = url_template.format(city)	
-	response = requests.get(url, params=payload)
-	response.raise_for_status()
-	print(response.text) 
+def main:
+	for city in cities:
+		url = url_template.format(city)	
+		response = requests.get(url, params=payload)
+		response.raise_for_status()
+		print(response.text) 
+
+
+if __name__ == '__main__'
+	main()
